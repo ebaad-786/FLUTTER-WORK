119bb0733e75182bc543957cd8021daf3cf8e1c5
Ebaad Kashif <ebadmotan@gmail.com>
2024-03-09 22:02:44 +0500

    learning

---
+++ assinment_3 .dart
@@ -0,0 +1,22 @@
+
+ void main() {
+  
+ num integer=-1;
+   
+  if (integer<0){
+    print ("negetive");
+    
+  }
+  else{
+    if (integer==0){
+      print ("zero");
+    }
+    if (integer>0){
+      print ("positive");
+    }
+    
+  } 
+   
+   
+  }
+
