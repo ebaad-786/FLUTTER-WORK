960339c2125a2af664d86830c146df3f564fad93
Ebaad Kashif <ebadmotan@gmail.com>
2024-03-23 10:51:02 +0500

    learning list

---
+++ assingment_5.dart
@@ -0,0 +1,23 @@
+//question no 1 //
+void main(){
+
+List<String> fruits = ["apple", "apricot","peach", "mango" ];
+
+ fruits.sort();
+
+ print(fruits);
+
+//question no 2 //
+
+List <int> evensnumbers =[];
+
+for (int i = 1; i <= 20; i++) 
+    if (i % 2 == 0) {
+      evensnumbers.add(i);
+    }
+      print ("the even num are ${evensnumbers}");
+
+}
+
+
+
