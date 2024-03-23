47d51b8efa64e69f1714ff7c185648a378db1547
Ebaad Kashif <ebadmotan@gmail.com>
2024-03-24 00:42:46 +0500

    learning list

--- assingment_5.dart
+++ assingment_5.dart
@@ -17,6 +17,54 @@ for (int i = 1; i <= 20; i++)
     }
       print ("the even num are ${evensnumbers}");
 
+      //question num 3//
+
+      List numbers =[2,3,4,2,5,1,3,5];
+
+      List uniquenumbers=numbers.toSet().toList(); 
+
+       uniquenumbers.sort();
+
+       print("this is list with duplicate numbers$numbers");
+       print("this is list without duplicate numbers$uniquenumbers");
+       
+       //question number 4//
+
+       List abc=[24,34,54];
+
+       print(abc.isEmpty);
+
+       //question 5//
+
+       List<String> fr1 = ["apple", "apricot","peach", "mango" ];
+       List<String> fr2 = ["cherry", "watermelon","guvava", "banana" ];
+
+       var fruitss ={...fr1, ...fr2};
+
+       print ("merged list is$fruitss");
+
+       //question number 6//
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
