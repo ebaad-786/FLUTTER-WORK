c3943838df79d217866ba07f04d4ac5662e930b2
Ebaad Kashif <ebadmotan@gmail.com>
2024-03-31 18:08:30 +0500

    list learning

--- assingment_5.dart
+++ assingment_5.dart
@@ -3,49 +3,64 @@ void main(){
 
 List<String> fruits = ["apple", "apricot","peach", "mango" ];
 
- fruits.sort();
+fruits.sort();
 
- print(fruits);
+print(fruits);
 
 //question no 2 //
 
 List <int> evensnumbers =[];
 
 for (int i = 1; i <= 20; i++) 
-    if (i % 2 == 0) {
-      evensnumbers.add(i);
-    }
-      print ("the even num are ${evensnumbers}");
+if (i % 2 == 0) {
+evensnumbers.add(i); }
+print ("the even num are ${evensnumbers}");
 
-      //question num 3//
+//question num 3//
 
-      List numbers =[2,3,4,2,5,1,3,5];
+List numbers =[2,3,4,2,5,1,3,5];
 
-      List uniquenumbers=numbers.toSet().toList(); 
+List uniquenumbers=numbers.toSet().toList(); 
 
-       uniquenumbers.sort();
+uniquenumbers.sort();
 
-       print("this is list with duplicate numbers$numbers");
-       print("this is list without duplicate numbers$uniquenumbers");
+print("this is list with duplicate numbers$numbers");
+print("this is list without duplicate numbers$uniquenumbers");
        
-       //question number 4//
+//question number 4//
 
-       List abc=[24,34,54];
+List abc=[24,34,54];
 
-       print(abc.isEmpty);
+print(abc.isEmpty);
 
-       //question 5//
+//question 5//
 
-       List<String> fr1 = ["apple", "apricot","peach", "mango" ];
-       List<String> fr2 = ["cherry", "watermelon","guvava", "banana" ];
+List<String> fr1 = ["apple", "apricot","peach", "mango" ];
+List<String> fr2 = ["cherry", "watermelon","guvava", "banana" ];
 
-       var fruitss ={...fr1, ...fr2};
+var fruitss ={...fr1, ...fr2};
 
-       print ("merged list is$fruitss");
+print ("merged list is$fruitss");
 
-       //question number 6//
+//question number 6//
 
+Map studentinfo={"name":"ebaad","age":"20","grade":"A+"};
+
+print (studentinfo); 
        
+//question number 7//
+
+Map population={"karchi":"2000000","lahore":"5000000","peshawar":"3000000"};
+
+List populationarrangment= population.values.toList();
+populationarrangment.sort();
+
+print ("The largest population is: ${population.keys.firstWhere((key) => population[key] ==
+populationarrangment.reversed.first)}");
+
+  
+
+      
 
 
 
